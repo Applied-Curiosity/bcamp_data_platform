7c1d4b0bd53d1d1eb21f97ea6db8f01eb9e40904
ty-revering <tyler.revering@appliedcuriosity.ai>
2024-05-31 03:06:35 +0000

    resoure group and storage account

---
+++ infra/dto.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass, field
+from typing import List
+
+
+# network configuration
+@dataclass
+class SubnetConfig:
+    name: str
+    address_prefix: str
+
+@dataclass
+class NSGRuleConfig:
+    name: str
+    priority: int
+    direction: str
+    access: str
+    protocol: str
+    source_port_range: str
+    destination_port_range: str
+    source_address_prefix: str
+    destination_address_prefix: str
+
+@dataclass
+class NetworkConfig:
+    vnet_name: str
+    address_space: str
+    subnets: List[SubnetConfig]
+    nsg: List[NSGRuleConfig]
